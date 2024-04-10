cb5c3f0494507fd5ac6aa2e55a14723a753419ef
Lucas Rennó Kallás <lucasrenn@hotmail.com>
2024-04-10 18:36:45 -0300

    feat: added qr code gen

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# QrCodeGen
+# Create a QR Code
+
 If is given a message, it would create a QR Code
+
+## Usage
+
+Change saveQRCodeImage function to receive the message that would be transformed in a qr code and pass the width of the square
+For exemple:
+
+saveQRCodeImage("message", 350);
+
+## Install
+
+Requirements: node
+
+Command: npm install
+To run: npm start
---
+++ index.js
@@ -0,0 +1,37 @@
+const qr = require("qrcode");
+const { promisify } = require("util");
+const fs = require("fs");
+
+// Transforma o retorno em Promise
+const toDataURLAsync = promisify(qr.toDataURL);
+
+const generateQRCode = async (texto, tamanho) => {
+	try {
+		const url = await toDataURLAsync(texto, {
+			width: tamanho,
+			height: tamanho,
+		});
+
+		const base64Data = url.split("base64,")[1];
+		return base64Data;
+	} catch (err) {
+		console.error("Erro ao gerar QR code:", err);
+		throw err;
+	}
+};
+
+const saveQRCodeImage = async (texto, tamanho) => {
+	try {
+		const base64Data = await generateQRCode(texto, tamanho);
+
+		const buffer = Buffer.from(base64Data, "base64");
+
+		fs.writeFileSync("myQrCode.png", buffer);
+		console.log("QR Code salvo com sucesso!");
+	} catch (err) {
+		console.error("Erro ao salvar QR Code:", err);
+	}
+};
+
+// text, width x height (square)
+saveQRCodeImage("texto", 350);
